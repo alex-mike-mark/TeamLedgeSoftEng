03b8db219378c414db6044e7ba7a74a375e1ecb3
Cole <kehlerc5@myumanitoba.ca>
2017-05-31 20:53:41 -0500

    Removed workoutID as per group discussion. (Also removed id from exercise class in previous commit, but forgot to comment). Added JavaDoc comments to constructor, methods.

--- MyApplication/app/src/main/java/ledge/muscleup/model/Workout.java
+++ MyApplication/app/src/main/java/ledge/muscleup/model/Workout.java
@@ -19,7 +19,6 @@
 package ledge.muscleup.model;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,74 +26,93 @@ import java.util.Map;
  */
 
 public class Workout {
-    private static int nextID = 0;
-    private int workoutID;
     private String name;
     private boolean isCustom;
     private boolean isFavourite;
     private Map<Exercise, ExerciseQuantity> exercises;
 
     /**
-     *
-     * @param name
-     * @param isCustom
+     * Constructor for a Workout
+     * @param name the name of the workout
+     * @param isCustom true if workout is a custom created workout, false otherwise
      */
     public Workout(String name, boolean isCustom){
-        workoutID = nextID;
-        nextID++;
         this.name = name;
         this.isCustom = isCustom;
         isFavourite = false;
         exercises = new HashMap<>();
     }
 
-    public int getWorkoutID() {
-        return workoutID;
-    }
-
-    public void setWorkoutID(int workoutID) {
-        this.workoutID = workoutID;
-    }
-
+    /**
+     * This returns the name of the workout
+     * @return the name of the workout
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * This sets the name of the workout to be parameter name
+     * @param name the new name of the workout
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This returns a boolean indicating whether this workout is a custom created one or not
+     * @return true if workout is a custom workout, false otherwise
+     */
     public boolean isCustom() {
         return isCustom;
     }
 
+    /**
+     * This sets whether the workout is custom or not
+     * @param custom a boolean indicating whether the workout is a custom workout or not
+     */
     public void setCustom(boolean custom) {
         isCustom = custom;
     }
 
+    /**
+     * This returns a boolean indicating whether this workout is a favourited workout
+     * @return true if workout is a favourite workout, false otherwise
+     */
     public boolean isFavourite() {
         return isFavourite;
     }
 
-    public void setFavourite(boolean favourite) {
-        isFavourite = favourite;
+    /**
+     * This toggles whether the workout is favourite or not. If it was not favourite, it becomes
+     * favourite and vice versa
+     */
+    public void toggleFavourite() {
+       this.isFavourite = !this.isFavourite;
     }
 
+    /**
+     * This returns the exercises in the workout, along with the quantities of each to be done in
+     * the workout in the form of a map
+     * @return A map with exercises in the workout as the keys, and the quantities of them to be done
+     * as the values
+     */
     public Map<Exercise, ExerciseQuantity> getExercises() {
         return exercises;
     }
 
     /**
-     *
-     * @param exercise
+     * Adds a new exercise to the workout, along with the quantity of the exercise to do
+     * @param exercise the exercise to be added
+     * @param quantity the quantity of this new exercise to do
      */
     public void addExercise(Exercise exercise, ExerciseQuantity quantity){
         exercises.put(exercise, quantity);
     }
 
     /**
-     *
-     * @param exercise
+     * Removes an exercise from the workout, if it exists
+     * @param exercise- the exercise to remove from the workout
      */
     public void removeExercise(Exercise exercise){
         exercises.remove(exercise);
