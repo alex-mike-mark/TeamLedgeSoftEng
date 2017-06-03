e419a5cf9d6110b87604a87126493e00c0294c9d
Cole <kehlerc5@myumanitoba.ca>
2017-06-02 23:45:56 -0500

    Create interfaces for a workout and an exercise

---
+++ MyApplication/app/src/main/java/ledge/muscleup/model/InterfaceExercise.java
@@ -0,0 +1,43 @@
+package ledge.muscleup.model;
+
+/**
+ * The interface for an exercise, which consists of a name, an intensity, a type and a recommended
+ * quantity of exercise
+ *
+ * @author Cole Kehler
+ * @version 1.0
+ * @since 2017-06-02.
+ */
+
+public interface InterfaceExercise {
+    /**
+     * Returns the name of the exercise
+     * @return the name of the exercise
+     */
+    String getName();
+
+    /**
+     * Returns the intensity of the exercise
+     * @return the intensity of the exercise
+     */
+    ExerciseIntensity getIntensity();
+
+    /**
+     * Returns the type of the exercise
+     * @return the type of the exercise
+     */
+    ExerciseType getType();
+
+    /**
+     * Returns the recommended quantity of exercise for the exercise
+     * @return the recommended quantity of exercise
+     */
+    ExerciseQuantity getRecommendedQuantity();
+
+    /**
+     * Compares the current InterfaceExercise to another instance of InterfaceExercise
+     * @param other the instance of InterfaceExercise to compare to
+     * @return a boolean representing whether the two instances were equal
+     */
+    boolean equals(InterfaceExercise other);
+}
---
+++ MyApplication/app/src/main/java/ledge/muscleup/model/InterfaceWorkout.java
@@ -0,0 +1,44 @@
+package ledge.muscleup.model;
+
+import java.util.NoSuchElementException;
+
+/**
+ * The interface for a workout, which consists of a workout name and a list of exercises
+ *
+ * @author Cole Kehler
+ * @version 1.0
+ * @since 2017-06-02.
+ */
+
+public interface InterfaceWorkout {
+    /**
+     * Returns the name of the workout
+     * @return the name of the workout
+     */
+    String getName();
+
+    /**
+     * Initializes the iterator for the list of exercises to the first InterfaceExercise in the list
+     */
+    void initExerciseIteration();
+
+    /**
+     * Returns {@code true} if there is another InterfaceExercise in the list or {@code false} if not
+     * @return a boolean representing whether the iterator is at the end of the list or not
+     */
+    boolean hasNextExercise();
+
+    /**
+     * Returns the next InterfaceExercise in the list
+     * @return the next InterfaceExercise in the list
+     * @throws NoSuchElementException if the iterator is at the end of the list
+     */
+    InterfaceExercise nextExercise() throws NoSuchElementException;
+
+    /**
+     * Compares the current InterfaceWorkout to another instance of InterfaceWorkout
+     * @param other the instance of InterfaceWorkout to compare to
+     * @return a boolean representing whether the two instances were equal
+     */
+    boolean equals(InterfaceWorkout other);
+}
