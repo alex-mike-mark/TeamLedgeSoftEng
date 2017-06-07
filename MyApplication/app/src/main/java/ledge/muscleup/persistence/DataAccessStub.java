6be2b63048d5c43082c382c212d366d2f68bed12
Jon <ingramj@myumanitoba.ca>
2017-06-06 20:54:22 -0500

    removed Log

--- MyApplication/app/src/main/java/ledge/muscleup/persistence/DataAccessStub.java
+++ MyApplication/app/src/main/java/ledge/muscleup/persistence/DataAccessStub.java
@@ -205,9 +205,7 @@ public class DataAccessStub implements DataAccess{
 
         if (workoutsByName.get(workout.getName()) != null) {
             InterfaceWorkout dbWorkout = workoutsByName.get(workout.getName());
-            Log.d("Workout", "Instance: " + (dbWorkout instanceof ModifiableWorkout));
             if (dbWorkout instanceof ModifiableWorkout) {
-                Log.d("Workout", "Exercise: " + exercisesByName.get(exercise.getName()));
                 if (exercisesByName.get(exercise.getName()) != null) {
                     InterfaceSuggestedExercise suggestedExercise = new SuggestedExercise(
                             exercise.getName(),
