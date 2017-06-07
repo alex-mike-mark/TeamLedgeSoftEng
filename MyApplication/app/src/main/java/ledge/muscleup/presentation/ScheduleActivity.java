8dceb745162b6033d5f47e4b307e0c2705e73535
AlexanderMark <marka3@myumantioba.ca>
2017-06-07 12:14:00 -0500

    Create ScheduleActivityClass

---
+++ MyApplication/app/src/main/java/ledge/muscleup/presentation/ScheduleActivity.java
@@ -0,0 +1,10 @@
+package ledge.muscleup.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alexander on 2017-06-07.
+ */
+
+public class ScheduleActivity extends AppCompatActivity {
+}
