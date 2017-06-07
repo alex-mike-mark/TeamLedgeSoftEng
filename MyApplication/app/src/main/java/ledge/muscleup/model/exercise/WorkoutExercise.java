73575a0d91d199a96ca64c64d4a09366a6f35380
Cole <kehlerc5@myumanitoba.ca>
2017-06-07 15:12:01 -0500

    Remove interface extensions in model classes

--- MyApplication/app/src/main/java/ledge/muscleup/model/exercise/WorkoutExercise.java
+++ MyApplication/app/src/main/java/ledge/muscleup/model/exercise/WorkoutExercise.java
@@ -8,7 +8,7 @@ package ledge.muscleup.model.exercise;
  * @since 2017-06-03
  */
 
-public class WorkoutExercise implements InterfaceWorkoutExercise {
+public class WorkoutExercise {
     private Exercise exercise;
     private InterfaceExerciseQuantity recommendedQuantity;
 
@@ -55,7 +55,6 @@ public class WorkoutExercise implements InterfaceWorkoutExercise {
      *
      * @return the name of the exercise
      */
-    @Override
     public String getName() {
         return exercise.getName();
     }
@@ -65,7 +64,6 @@ public class WorkoutExercise implements InterfaceWorkoutExercise {
      *
      * @return the intensity of the exercise
      */
-    @Override
     public ExerciseIntensity getIntensity() {
         return exercise.getIntensity();
     }
@@ -75,7 +73,6 @@ public class WorkoutExercise implements InterfaceWorkoutExercise {
      *
      * @return the type of the exercise
      */
-    @Override
     public ExerciseType getType() {
         return exercise.getType();
     }
@@ -85,7 +82,6 @@ public class WorkoutExercise implements InterfaceWorkoutExercise {
      *
      * @return the recommended quantity of exercise
      */
-    @Override
     public InterfaceExerciseQuantity getRecommendedQuantity() { return recommendedQuantity; }
 
     /**
@@ -95,7 +91,6 @@ public class WorkoutExercise implements InterfaceWorkoutExercise {
      * @throws IllegalArgumentException if passed a {@code null} parameter
      * @return a boolean representing if the suggested quantity could be updated
      */
-    @Override
     public boolean updateRecommendedQuantity(InterfaceExerciseQuantity quantity) throws IllegalArgumentException {
         boolean quantityUpdated = false;
 
@@ -111,13 +106,12 @@ public class WorkoutExercise implements InterfaceWorkoutExercise {
     }
 
     /**
-     * Compares the current WorkoutExercise to another instance of InterfaceWorkoutExercise
+     * Compares the current WorkoutExercise to another instance of WorkoutExercise
      *
-     * @param other the instance of InterfaceWorkoutExercise to compare to
+     * @param other the instance of WorkoutExercise to compare to
      * @return a boolean representing whether the two instances were equal
      */
-    @Override
-    public boolean equals(InterfaceWorkoutExercise other){
+    public boolean equals(WorkoutExercise other){
         return (other != null &&
                 getName().equals(other.getName()) &&
                 recommendedQuantity.equals(other.getRecommendedQuantity()));
