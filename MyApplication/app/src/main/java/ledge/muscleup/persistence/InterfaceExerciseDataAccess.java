db6f756982405a2813ca0407dbc874b89a3d1695
Ryan Koop <koopr@myumanitoba.ca>
2017-06-07 01:23:47 -0500

    Added JavaDoc comments for all interface methods in the persistence layer

--- MyApplication/app/src/main/java/ledge/muscleup/persistence/InterfaceExerciseDataAccess.java
+++ MyApplication/app/src/main/java/ledge/muscleup/persistence/InterfaceExerciseDataAccess.java
@@ -13,9 +13,35 @@ import ledge.muscleup.model.exercise.InterfaceExercise;
  * @since 2017-06-07
  */
 public interface InterfaceExerciseDataAccess {
+
+    /**
+     * Gets a list of all exercises in the database
+     * @return a list of all exercises in the database
+     */
     List<InterfaceExercise> getExercisesList();
+
+    /**
+     * Gets a list of names of all exercises in the database
+     * @return a list of names of all exercises in the database
+     */
     List<String> getExerciseNamesList();
+
+    /**
+     * Retrieves an exercise from the database with the name given as parameter
+     * @param exerciseName- the name of the exercise to retrieve from the database
+     * @return The exercise with name exerciseName, or null if no exercise exists with that name
+     */
     InterfaceExercise getExercise(String exerciseName);
+
+    /**
+     * Adds an exercise to the database
+     * @param exercise the exercise to be added to the database
+     */
     void insertExercise(InterfaceExercise exercise);
+
+    /**
+     * Removes an exercise from the database, if it exists
+     * @param exercise the exercise to remove from the database
+     */
     void removeExercise(InterfaceExercise exercise);
 }
