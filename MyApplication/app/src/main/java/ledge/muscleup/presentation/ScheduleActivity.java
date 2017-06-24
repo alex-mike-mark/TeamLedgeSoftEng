a9c9ccf47eaaa38e01ccdf85e79ea02dcd0fce99
Ryan Koop <koopr@myumanitoba.ca>
2017-06-23 23:30:54 -0500

    Added listeners to items in the ScheduleActivity to open a WorkoutSessionActivity when clicking on a session

--- MyApplication/app/src/main/java/ledge/muscleup/presentation/ScheduleActivity.java
+++ MyApplication/app/src/main/java/ledge/muscleup/presentation/ScheduleActivity.java
@@ -1,11 +1,19 @@
 package ledge.muscleup.presentation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import ledge.muscleup.R;
@@ -14,6 +22,7 @@ import ledge.muscleup.business.AccessWorkoutSessions;
 import ledge.muscleup.business.InterfaceAccessWorkoutSessions;
 import ledge.muscleup.business.InterfaceScheduleManager;
 import ledge.muscleup.business.ScheduleManager;
+import ledge.muscleup.model.workout.WorkoutSession;
 import ledge.muscleup.persistence.DataAccessStub;
 import ledge.muscleup.persistence.InterfaceDataAccess;
 
@@ -30,7 +39,7 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         InterfaceScheduleManager scheduleManager = new ScheduleManager(new AccessWorkoutSessions());
         ListManager lm = new ListManager();
-        List scheduleArray;
+        List<WorkoutSession> scheduleArray;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_display);
@@ -38,6 +47,23 @@ public class ScheduleActivity extends AppCompatActivity {
         scheduleArray = scheduleManager.getWorkoutSessionList();
 
         lm.populateList(this, scheduleArray);
+        setupListeners(scheduleArray);
     }
 
+    private void setupListeners(final List<WorkoutSession> workoutSessionList) {
+        ListView list = (ListView) findViewById(R.id.list_panel);
+
+        //will respond to clicks by opening a WorkoutSessionActivity and passing the date of the session clicked
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+                Intent appInfo = new Intent(ScheduleActivity.this, WorkoutSessionActivity.class);
+                LocalDate date = workoutSessionList.get(position).getDate();
+                DateTimeFormatter  formatter= DateTimeFormat.forPattern("MM/dd/yyyy");
+
+                appInfo.putExtra("workoutSessionDate", formatter.print(date));
+                startActivity(appInfo);
+            }
+        });
+    }
 }
