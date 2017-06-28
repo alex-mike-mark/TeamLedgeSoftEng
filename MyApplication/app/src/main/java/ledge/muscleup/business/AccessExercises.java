f569b1aa18edb70645f3a265082dce6364cd0e0a
Anthrasite <kehlerc5@myumanitoba.ca>
2017-06-28 15:32:02 -0500

    Remove unused access methods

--- MyApplication/app/src/main/java/ledge/muscleup/business/AccessExercises.java
+++ MyApplication/app/src/main/java/ledge/muscleup/business/AccessExercises.java
@@ -25,43 +25,10 @@ public class AccessExercises implements InterfaceAccessExercises {
     }
 
     /**
-     * This method gets an exercise from the database with the given name
-     * @param exerciseName the name of the exercise
-     * @return an exercise from the database with the given name, or null if it does not exist
-     */
-    public Exercise getExercise(String exerciseName) {
-        return dataAccess.getExercise(exerciseName);
-    }
-
-    /**
      * This method gets exercises stored in the database in the form of a list
      * @return a list of exercises in the database
      */
     public List<Exercise> getExercisesList() {
         return dataAccess.getExercisesList();
-    }
-
-    /**
-     * This method gets the names of all exercises in the database in the form of a list
-     * @return a list of exercise names in the database
-     */
-    public List<String> getExerciseNamesList() {
-        return dataAccess.getExerciseNamesList();
-    }
-
-    /**
-     * This method inserts a new exercise into the database
-     * @param exercise the exercise to be inserted
-     */
-    public void insertExercise(Exercise exercise) {
-        dataAccess.insertExercise(exercise);
-    }
-
-    /**
-     * This method removes an exercise from the database
-     * @param exercise the exercise to be removed
-     */
-    public void removeExercise(Exercise exercise) {
-        dataAccess.removeExercise(exercise);
     }
 }
