d0188bd67a90fe7624ccafc9471ecc30ba7ce0fe
Anthrasite <kehlerc5@myumanitoba.ca>
2017-06-28 15:48:51 -0500

    Remove unused database access methods

--- MyApplication/app/src/main/java/ledge/muscleup/persistence/InterfaceExerciseDataAccess.java
+++ MyApplication/app/src/main/java/ledge/muscleup/persistence/InterfaceExerciseDataAccess.java
@@ -19,29 +19,4 @@ public interface InterfaceExerciseDataAccess {
      * @return a list of all exercises in the database
      */
     List<Exercise> getExercisesList();
-
-    /**
-     * Gets a list of names of all exercises in the database
-     * @return a list of names of all exercises in the database
-     */
-    List<String> getExerciseNamesList();
-
-    /**
-     * Retrieves an exercise from the database with the name given as parameter
-     * @param exerciseName- the name of the exercise to retrieve from the database
-     * @return The exercise with name exerciseName, or null if no exercise exists with that name
-     */
-    Exercise getExercise(String exerciseName);
-
-    /**
-     * Adds an exercise to the database
-     * @param exercise the exercise to be added to the database
-     */
-    void insertExercise(Exercise exercise);
-
-    /**
-     * Removes an exercise from the database, if it exists
-     * @param exercise the exercise to remove from the database
-     */
-    void removeExercise(Exercise exercise);
 }
