efba812dd73bc998fbf2b7b50ff9573d45c54adb
Cole <kehlerc5@myumanitoba.ca>
2017-07-08 15:06:10 -0500

    Implement the CompletedWorkoutRecord class to hold information about a completed workout

--- MyApplication/app/src/main/java/ledge/muscleup/model/experience/CompletedWorkoutRecord.java
+++ MyApplication/app/src/main/java/ledge/muscleup/model/experience/CompletedWorkoutRecord.java
@@ -1,5 +1,7 @@
 package ledge.muscleup.model.experience;
 
+import org.joda.time.LocalDate;
+
 /**
  * A record of a completed workout, which contains the date the workout was completed, the experience
  * gained from completing the workout, the experience value after completing the workout and the
@@ -11,4 +13,55 @@ package ledge.muscleup.model.experience;
  */
 
 public class CompletedWorkoutRecord {
+    private String workoutName;
+    private int experienceGained;
+    private int experienceAfterCompletion;
+    private LocalDate dateOfCompletion;
+
+    /**
+     * The default constructor for a CompletedWorkoutRecord
+     * @param workoutName the name of the workout
+     * @param experienceBeforeCompletion the experience before completing this workout
+     * @param experienceAfterCompletion the experience after completing this workout
+     * @param dateOfCompletion the date the workout was completed
+     */
+    public CompletedWorkoutRecord(String workoutName, int experienceBeforeCompletion,
+                                  int experienceAfterCompletion, LocalDate dateOfCompletion) {
+        this.workoutName = workoutName;
+        this.experienceAfterCompletion = experienceAfterCompletion;
+        this.experienceGained = experienceAfterCompletion - experienceBeforeCompletion;
+        this.dateOfCompletion = dateOfCompletion;
+    }
+
+    /**
+     * Returns the name of the workout
+     * @return the name of the workout
+     */
+    public String getWorkoutName() {
+        return workoutName;
+    }
+
+    /**
+     * Returns the experience gained from completing this workout
+     * @return the experience gained from completing this workout
+     */
+    public int getExperienceGained() {
+        return experienceGained;
+    }
+
+    /**
+     * Returns the experience gained after completing this workout
+     * @return the experience gained after completing this workout
+     */
+    public int getExperienceAfterCompletion() {
+        return experienceAfterCompletion;
+    }
+
+    /**
+     * Returns the date this workout was completed
+     * @return the date this workout was completed
+     */
+    public LocalDate getDateOfCompletion() {
+        return dateOfCompletion;
+    }
 }
