702b3913b38aeacf1ff4b3d37d2ff6aa8b08397f
Cole <kehlerc5@myumanitoba.ca>
2017-07-08 15:00:22 -0500

    Create classes for handling experience history

---
+++ MyApplication/app/src/main/java/ledge/muscleup/model/experience/CompletedWorkoutRecord.java
@@ -0,0 +1,14 @@
+package ledge.muscleup.model.experience;
+
+/**
+ * A record of a completed workout, which contains the date the workout was completed, the experience
+ * gained from completing the workout, the experience value after completing the workout and the
+ * name of the workout completed
+ *
+ * @author Cole Kehler
+ * @version 3.0
+ * @since 2017-07-08
+ */
+
+public class CompletedWorkoutRecord {
+}
---
+++ MyApplication/app/src/main/java/ledge/muscleup/model/experience/ExperienceHistory.java
@@ -0,0 +1,13 @@
+package ledge.muscleup.model.experience;
+
+/**
+ * A history of all workouts completed in a given time period and the current experience level of
+ * the user
+ *
+ * @author Cole Kehler
+ * @version 3.0
+ * @since 2017-07-08
+ */
+
+public class ExperienceHistory {
+}
