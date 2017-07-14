1abef1ed574e3a1d5efdca6f512e9cd9d3767280
Cole <kehlerc5@myumanitoba.ca>
2017-07-13 19:14:38 -0500

    Modify the Services class to hold each data access objects for each component of the database and allow access to them

--- MyApplication/app/src/main/java/ledge/muscleup/application/Services.java
+++ MyApplication/app/src/main/java/ledge/muscleup/application/Services.java
@@ -1,7 +1,16 @@
 package ledge.muscleup.application;
 
+import ledge.muscleup.business.InterfaceAccessWorkoutSessions;
 import ledge.muscleup.persistence.DataAccess;
+import ledge.muscleup.persistence.ExerciseDataAccess;
+import ledge.muscleup.persistence.ExperienceDataAccess;
 import ledge.muscleup.persistence.InterfaceDataAccess;
+import ledge.muscleup.persistence.InterfaceExerciseDataAccess;
+import ledge.muscleup.persistence.InterfaceExperienceDataAccess;
+import ledge.muscleup.persistence.InterfaceWorkoutDataAccess;
+import ledge.muscleup.persistence.InterfaceWorkoutSessionDataAccess;
+import ledge.muscleup.persistence.WorkoutDataAccess;
+import ledge.muscleup.persistence.WorkoutSessionDataAccess;
 
 /**
  * A class containing static methods for creating, retrieving, and closing the DataAccess.
@@ -13,44 +22,76 @@ import ledge.muscleup.persistence.InterfaceDataAccess;
  */
 public class Services {
     private static InterfaceDataAccess dataAccessService = null;
+    private static InterfaceExerciseDataAccess exerciseDataAccessService = null;
+    private static InterfaceExperienceDataAccess experienceDataAccessService = null;
+    private static InterfaceWorkoutDataAccess workoutDataAccessService = null;
+    private static InterfaceWorkoutSessionDataAccess workoutSessionDataAccessService = null;
 
     /**
-     * Creates and opens the database, if it hasn't already been created. Returns the stub
-     * database
+     * Creates and opens the database, if it hasn't already been created
      * @param dbName the name of the database
-     * @return the database
      */
-    public static InterfaceDataAccess createDataAccess(String dbName) {
+    public static void createDataAccess(String dbName) {
         if (dataAccessService == null) {
             dataAccessService = new DataAccess(dbName);
             dataAccessService.open(Main.getDBPathName());
+
+            exerciseDataAccessService = new ExerciseDataAccess();
+            experienceDataAccessService = new ExperienceDataAccess();
+            workoutDataAccessService = new WorkoutDataAccess();
+            workoutSessionDataAccessService = new WorkoutSessionDataAccess();
         }
-        return dataAccessService;
     }
 
     /**
      * Creates and opens the database, using an alternative database implementation
      * @param altDataAccessService the alternative database implementation
-     * @return the database
      */
-    public static InterfaceDataAccess createDataAccess(DataAccess altDataAccessService) {
+    public static void createDataAccess(InterfaceDataAccess altDataAccessService) {
         if (dataAccessService == null) {
             dataAccessService = altDataAccessService;
             dataAccessService.open(Main.getDBPathName());
         }
-        return dataAccessService;
     }
 
     /**
-     * Retrieves the database
-     * @return the database, if it exists
+     * Gets the class which gives access to exercise data
+     * @return access to exercise data
      */
-    public static InterfaceDataAccess getDataAccess() {
-        if (dataAccessService == null) {
-            System.out.println("Connection to data access has not been established.");
-            System.exit(1);
-        }
-        return dataAccessService;
+    public static InterfaceExerciseDataAccess getExerciseDataAccess() {
+        if (dataAccessService == null)
+            handleUninitializedDB();
+        return exerciseDataAccessService;
+    }
+
+    /**
+     * Gets the class which gives access to workout data
+     * @return access to workout data
+     */
+    public static InterfaceWorkoutDataAccess getWorkoutDataAccess() {
+        if (dataAccessService == null)
+            handleUninitializedDB();
+        return workoutDataAccessService;
+    }
+
+    /**
+     * Gets the class which gives access to workout session data
+     * @return access to workout session data
+     */
+    public static InterfaceWorkoutSessionDataAccess getWorkoutSessionDataAccess() {
+        if (dataAccessService == null)
+            handleUninitializedDB();
+        return workoutSessionDataAccessService;
+    }
+
+    /**
+     * Gets the class which gives access to experience data
+     * @return access to experience data
+     */
+    public static InterfaceExperienceDataAccess getExperienceDataAccess() {
+        if (dataAccessService == null)
+            handleUninitializedDB();
+        return experienceDataAccessService;
     }
 
     /**
@@ -60,7 +101,16 @@ public class Services {
     public static void closeDataAccess() {
         if (dataAccessService != null) {
             dataAccessService.close();
+            exerciseDataAccessService.close();
             dataAccessService = null;
         }
+    }
+
+    /**
+     * Handles cases where attempts are made to modify an uninitialized database
+     */
+    private static void handleUninitializedDB() {
+        System.out.println("Connection to data access has not been established");
+        System.exit(1);
     }
 }
